2230f3ce62e01858125cc165e3f2bb27937ef58c
Adrian <adr.fueller@gmail.com>
2022-12-22 21:50:12 +0100

    day 10 part 2

--- 10/main.py
+++ 10/main.py
@@ -20,7 +20,6 @@ while i < len(lines):
  
   if (cycle-20) % 40 == 0:
     sigStrength += cycle * X
-  # print(cycle, ":", X, "(", lines[i], ")")
 
   if lines[i].startswith('addx'):
     if watingCycle:
@@ -34,3 +33,45 @@ while i < len(lines):
     i += 1
 
 print(sigStrength)
+
+
+## Teil 2 ##
+
+X = 1
+cycle = 0
+i = 0
+sigStrength = 0
+watingCycle = True
+
+CRT = np.full((6, 40), " ")
+print(CRT)
+
+while i < len(lines):
+  cycle += 1
+  row = (cycle -1) // 40
+  col1 = (X -1) % 40
+  col2 = (X) % 40
+  col3 = (X +1) % 40
+  cyc2 = cycle % 40
+
+  if col1 == cyc2-1 or col2 == cyc2-1 or col3 == cyc2-1:
+    CRT[row][cyc2-1] = '#'
+
+  if lines[i].startswith('addx'):
+    if watingCycle:
+      watingCycle = False
+      continue
+    else:
+      watingCycle = True
+      X += int(lines[i].split(' ')[1])
+      i += 1
+  else:
+    i += 1
+
+print(CRT)
+CRTString = []
+for row in CRT:
+  CRTString.append(''.join(row))
+f = open(dir + "/lsgWort.txt", "w")
+f.write(str(CRTString))
+f.close()
