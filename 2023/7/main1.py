3e7549a8845cfce02b363de7f91274e7597127d7
AdrFue <adr.fueller@gmail.com>
2023-12-08 14:21:50 +0100

    day 7

--- 2023/7/main1.py
+++ 2023/7/main1.py
@@ -5,14 +5,11 @@ dir = os.path.dirname(os.path.realpath(__file__))
 file_name = ["\\input1Test.txt","\\input1.txt", "\\input2Test.txt", "\\input2.txt"]
 
 lines = []
-with open(dir + file_name[0], "r") as f:
+with open(dir + file_name[1], "r") as f:
     for line in f:
     	lines.append(line.strip())
 
-print("Lines:", lines)
 data = [line.split() for line in lines]
-pprint(data)
-print("="*80)
 
 data_t = {
     "FiveKind": [],
@@ -25,11 +22,12 @@ data_t = {
 }
 
 # Mapping
-# for i, d in enumerate(data):
-#     data[i][0] = d[0].replace("A", "E")
-#     data[i][0] = d[0].replace("K", "D")
-#     data[i][0] = d[0].replace("Q", "C")
-#     data[i][0] = d[0].replace("T", "A")
+for i, d in enumerate(data):
+    data[i][0] = d[0].replace("A", "E")
+    data[i][0] = d[0].replace("K", "D")
+    data[i][0] = d[0].replace("Q", "C")
+    data[i][0] = d[0].replace("J", "B")
+    data[i][0] = d[0].replace("T", "A")
 
 # card type
 for i, d in enumerate(data): 
@@ -39,48 +37,31 @@ for i, d in enumerate(data):
             ls[letter]+=1
         else:
             ls[letter]=1
-    print(ls)
+
     if 5 in ls.values():
-        print("FiveKind")
         data_t["FiveKind"].append(d[0])
         data[i].append("FiveKind")
-
     elif 4 in ls.values():
-        print("FourKind")   
         data_t["FourKind"].append(d[0])
         data[i].append("FourKind")
     elif 3 in ls.values():
         if 2 in ls.values():
-            print("FullHouse")
             data_t["FullHouse"].append(d[0])
             data[i].append("FullHouse")
         else:
-            print("ThreeKind")
             data_t["ThreeKind"].append(d[0])
             data[i].append("ThreeKind")
     elif 2 in ls.values():
         if len(ls) == 3:
-            print("TwoPair")
             data_t["TwoPair"].append(d[0])
             data[i].append("TwoPair")
         else:
-            print("OnePair")
             data_t["OnePair"].append(d[0])
             data[i].append("OnePair")
     else:
-        print("HighCard")
         data_t["HighCard"].append(d[0])
         data[i].append("HighCard")
-    
-    print(d[0])   
-
-print("="*80)
-pprint(data)
-print("="*80)
-pprint(data_t)
-print("="*80)
-
-
+  
 # ranking
 ranks = {
     "A": 14,
@@ -93,33 +74,16 @@ for i in range(9, 1, -1):
     ranks[str(i)] = i
 cur_rank = len(data)
 
-# comp_list = ""
-for key, value in data_t.items():
-    data_t[key] = list(map(list, zip(*value))) 
-pprint(data_t)
+for key, types in data_t.items():
+    type_s = sorted(types, reverse=True)
+    for type in type_s:
+        for i, d in enumerate(data):
+            if d[0] == type:
+                data[i].append(cur_rank)
+                cur_rank -= 1
 
-for key, values in data_t.items():
-    print("Type = Values:", values)
-    for value in values:
-        print("Erste Buchstaben = value: ", value)
-        found = 0
-        for rank in ranks:
-            for v in value:
-                if rank == v:
-                    found += 1
-            if found == 1:
-                print(1, rank, found)
-                break
-            if found > 1:
-                print(2, rank, found)
-                break
-        if found == 1:
-            break
-                
-
-# pprint(data)
-# result1 = 0
-# for d in data:
-#     result1 += int(d[1]) * int(d[2])
+result1 = 0
+for d in data:
+    result1 += int(d[1]) * int(d[3])
                             
-# print(result1)
+print(result1)
---
+++ 2023/7/main2.py
@@ -0,0 +1,99 @@
+import os 
+from pprint import pprint
+
+dir = os.path.dirname(os.path.realpath(__file__))
+file_name = ["\\input1myTest.txt","\\input1.txt", "\\input2Test.txt", "\\input2.txt"]
+
+lines = []
+with open(dir + file_name[1], "r") as f:
+    for line in f:
+    	lines.append(line.strip())
+
+# print("Lines:", lines)
+data = [line.split() for line in lines]
+# pprint(data)
+# print("="*80)
+
+data_t = {
+    "FiveKind": [],
+    "FourKind": [],
+    "FullHouse": [],
+    "ThreeKind": [],
+    "TwoPair": [],
+    "OnePair": [],
+    "HighCard": []
+}
+
+# Mapping
+for i, d in enumerate(data):
+    data[i][0] = d[0].replace("A", "E")
+    data[i][0] = d[0].replace("K", "D")
+    data[i][0] = d[0].replace("Q", "C")
+    data[i][0] = d[0].replace("J", "1")
+    data[i][0] = d[0].replace("T", "A")
+
+# card type + joker
+for i, d in enumerate(data): 
+    ls=dict()
+    for letter in d[0]:
+        if letter in ls:   
+            ls[letter]+=1
+        else:
+            ls[letter]=1
+    
+    # joker
+    jokers = ls.get("1")
+    if jokers is None:
+        jokers = 0
+    else:
+        jokers = int(jokers)
+    
+    if jokers > 1 and jokers != 5:
+        ls = dict(sorted(ls.items(), key=lambda item: item[1], reverse=True))
+        ls.pop('1', None)
+        ls[list(ls.keys())[0]] = ls[list(ls.keys())[0]]+jokers
+
+    if jokers == 1:
+        max_key = max(ls, key=ls.get)
+        ls[max_key] = ls.get(max_key)+1
+
+    # card type
+    if 5 in ls.values():
+        data_t["FiveKind"].append(d[0])
+        data[i].append("FiveKind")
+    elif 4 in ls.values():
+        data_t["FourKind"].append(d[0])
+        data[i].append("FourKind")
+    elif 3 in ls.values():
+        if 2 in ls.values():
+            data_t["FullHouse"].append(d[0])
+            data[i].append("FullHouse")
+        else:
+            data_t["ThreeKind"].append(d[0])
+            data[i].append("ThreeKind")
+    elif 2 in ls.values():
+        if len(ls) == 3:
+            data_t["TwoPair"].append(d[0])
+            data[i].append("TwoPair")
+        else:
+            data_t["OnePair"].append(d[0])
+            data[i].append("OnePair")
+    else:
+        data_t["HighCard"].append(d[0])
+        data[i].append("HighCard")
+  
+# ranking
+cur_rank = len(data)
+for key, types in data_t.items():
+    type_s = sorted(types, reverse=True)
+    for type in type_s:
+        for i, d in enumerate(data):
+            if d[0] == type:
+                data[i].append(cur_rank)
+                cur_rank -= 1
+
+result1 = 0
+for d in data:
+    result1 += int(d[1]) * int(d[3])
+                            
+print(result1)
