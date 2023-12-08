d1082a1cc4afc872bd76cb9636ba62f36c3ae1ed
AdrFue <adr.fueller@gmail.com>
2023-12-08 22:42:30 +0100

    day 6 minified on three lines

--- 2023/6/main.min.py
+++ 2023/6/main.min.py
@@ -1,8 +1,3 @@
-import os 
-import math
-with open(os.path.dirname(os.path.realpath(__file__)) + "\\input1.txt", "r") as f:
-    lines = [line.strip() for line in f]
-def solve(d):
-    return [sum(1 for i in range(int(race[0]) + 1) if i * (int(race[0]) - i) > int(race[1])) for race in d]
-print(math.prod(solve(list(map(list, zip(*[line.split()[1:] for line in lines])))))) ## Part 1
-print(solve([["".join(d) for d in [line.split()[1:] for line in lines]]])[0]) ## Part 2
+import os, math
+with open(os.path.dirname(os.path.realpath(__file__)) + "\\input1.txt", "r") as f: lines = [line.strip() for line in f]
+print(math.prod([sum(1 for i in range(int(race[0]) + 1) if i*(int(race[0])-i)>int(race[1])) for race in list(map(list, zip(*[l.split()[1:] for l in lines])))]),[sum(1 for i in range(int(race[0])+1) if i*(int(race[0])-i)>int(race[1])) for race in [["".join(d) for d in [l.split()[1:] for l in lines]]]][0])
